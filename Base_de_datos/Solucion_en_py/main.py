98667f591b5882b9541ed5bdc3ae6a4f9cff6fb3
Your Name <janreigjane2@gmail.com>
2023-05-08 21:13:18 +0200

    first commit

---
+++ Base_de_datos/Solucion_en_py/main.py
@@ -0,0 +1,8 @@
+def invertir_texto(texto):
+    return texto[::-1]
+
+texto_propuesto = "Hola mundo"
+solucion = invertir_texto(texto_propuesto)
+
+print("Texto propuesto: ", texto_propuesto)
+print("Solución: ", solucion)
