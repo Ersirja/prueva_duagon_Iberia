23906725abcc4de7b5c9a2b4ee65fe564cc9d402
Your Name <janreigjane2@gmail.com>
2023-05-08 21:22:00 +0200

    segundo commit

--- Base_de_datos/Solucion_en_py/main.py
+++ Base_de_datos/Solucion_en_py/main.py
@@ -1,7 +1,7 @@
 def invertir_texto(texto):
     return texto[::-1]
 
-texto_propuesto = "Hola mundo"
+texto_propuesto = "Hola mundo, este es un texto para darle la vuelta"
 solucion = invertir_texto(texto_propuesto)
 
 print("Texto propuesto: ", texto_propuesto)
---
+++ Inversion_de_texto/Solucion_en_.net/prueba/prueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string texto_propuesto = "Hola mundo";
+        string solucion = InvertirTexto(texto_propuesto);
+
+        Console.WriteLine("Texto propuesto: " + texto_propuesto);
+        Console.WriteLine("Solución: " + solucion);
+    }
+
+    public static string InvertirTexto(string texto)
+    {
+        char[] caracteres = texto.ToCharArray();
+        Array.Reverse(caracteres);
+        return new string(caracteres);
+    }
+}
